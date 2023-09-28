6608bf6ce04e2611d3f554c645f7b11339b3ab9e
connor-griffin <144114400+connor-griffin@users.noreply.github.com>
2023-09-28 02:42:20 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const tmdbKey = 'eab4aa935a838ff980f10a969758b4bf';
+const tmdbBaseUrl = 'https://api.themoviedb.org/3';
+const playBtn = document.getElementById('playBtn');
+
+const getGenres = async () => {
+  const genreRequestEndpoint = 'genre/movie/list';
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
+  try {g
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      const genres = jsonResponse.genres;
+      return genres;
+    }
+  } catch (error){
+      console.log(error);
+  };
+};
+
+const getMovies = () => {
+  const selectedGenre = getSelectedGenre();
+
+};
+
+const getMovieInfo = () => {
+
+};
+
+// Gets a list of movies and ultimately displays the info of a random movie from the list
+const showRandomMovie = () => {
+  const movieInfo = document.getElementById('movieInfo');
+  if (movieInfo.childNodes.length > 0) {
+    clearCurrentMovie();
+  };
+
+};
+
+getGenres().then(populateGenreDropdown);
+playBtn.onclick = showRandomMovie;
